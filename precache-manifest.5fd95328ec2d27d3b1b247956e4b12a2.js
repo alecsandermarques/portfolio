c4f5d7e98753a648ac0b19488815e616fddd81f4
Alecsander Marques <alecsander.marques@softplan.com.br>
2020-03-24 20:36:35 -0300

    Updates

---
+++ precache-manifest.5fd95328ec2d27d3b1b247956e4b12a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d59ed120a54213b3168bd586b101a5e",
+    "url": "/alecsandermarques.com/portfolio/index.html"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/alecsandermarques.com/portfolio/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alecsandermarques.com/portfolio/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9caa75d33ccd05deee14",
+    "url": "/alecsandermarques.com/portfolio/static/js/main.6d4966a2.chunk.js"
+  },
+  {
+    "revision": "323f0598bbd171149769",
+    "url": "/alecsandermarques.com/portfolio/static/js/runtime-main.6cea8ccf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f106369f3bf0207ff54eb48e973738e3.js"
+  "/alecsandermarques.com/portfolio/precache-manifest.5fd95328ec2d27d3b1b247956e4b12a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alecsandermarques.com/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
