b029e79c4b2180d5969272356bb35011d5dc9a77
Alecsander Marques <alecsander.marques@softplan.com.br>
2020-03-25 16:16:57 -0300

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import colors from '../../colors';
+import Menu from '../Menu';
+import { Container, Content } from './HeaderStyle';
 
-const NotFound = () => <h1>Header</h1>;
+export default function Header() {
+  const [width, setWidth] = useState([0]);
 
-export default NotFound;
+  useLayoutEffect(() => {
+    function updateWidth() {
+      setWidth([window.innerWidth]);
+    }
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
+    return () => window.removeEventListener('resize', updateWidth);
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        {width < 769 ? (
+          <nav>
+            <Menu />
+            <img src={logo} alt="logo" />
+          </nav>
+        ) : (
+          <nav>
+            <img src={logo} alt="logo" />
+            <NavLink activeStyle={{ color: `${colors.primary}` }} to="/home">
+              HOME
+            </NavLink>
+            <NavLink activeStyle={{ color: `${colors.primary}` }} to="/about">
+              ABOUT ME
+            </NavLink>
+            <NavLink activeStyle={{ color: `${colors.primary}` }} to="/skills">
+              SKILLS
+            </NavLink>
+          </nav>
+        )}
+      </Content>
+    </Container>
+  );
+}
