66f3d97719f03cb7371c70046e400366fb3cd419
Alecsander Marques <alecsander.marques@softplan.com.br>
2020-04-05 13:09:05 -0300

    Updates

--- precache-manifest.8daf7eb9ab1f994bfe8ed3d765304ee0.js
+++ precache-manifest.8daf7eb9ab1f994bfe8ed3d765304ee0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5215149c46e5fe8b53e3bafabbcd8dce",
+    "revision": "edfeec12b1d2856457cef0262cfd4ceb",
     "url": "/index.html"
   },
   {
-    "revision": "e333e96eba025fc0c25a",
+    "revision": "a78a04073233c2a9579a",
     "url": "/static/css/main.50649732.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0571658c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e333e96eba025fc0c25a",
-    "url": "/static/js/main.dbb886e5.chunk.js"
+    "revision": "a78a04073233c2a9579a",
+    "url": "/static/js/main.807e2aa3.chunk.js"
   },
   {
     "revision": "5284eec07972ace79c95",
